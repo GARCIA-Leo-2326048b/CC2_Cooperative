d03b573572e0488b09f263d241fcda3e5a21f10b
Garcia-Leo09 <leo.garcia.5@etu.univ-amu.fr>
2025-04-02 19:31:16 +0200

    initialisation du projet

---
+++ src/main/java/com/produitutilisateur/produitutilisateur/ProduitUtilisateurApplication.java
@@ -0,0 +1,9 @@
+package com.produitutilisateur.produitutilisateur;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class ProduitUtilisateurApplication extends Application {
+
+}
